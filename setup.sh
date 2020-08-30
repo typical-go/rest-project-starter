30b60e2f8fa0a702d1216e59a4cd8963927a156d
iman tung <iman.tung@gmail.com>
2020-08-30 18:20:30 +0700

    remove setup.sh instead of install.sh

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ set -euxo pipefail
 
 git clone git@github.com:typical-go/rest-project-starter.git $1
 rm -rf $1/.git
-rm -rf $1/install.sh $1/README.md
+rm -rf $1/setup.sh $1/README.md
 find $1 -type f -exec sed -i '' "s+github.com/typical-go/rest-project-starter+$2+g" {} \;
 find $1 -type f -exec sed -i '' "s+rest-project-starter+$1+g" {} \;
 mv $1/cmd/rest-project-starter $1/cmd/$1
