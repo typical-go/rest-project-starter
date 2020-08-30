5fc651f3d21d652f7f3d023a1439e616558ee0df
iman tung <iman.tung@gmail.com>
2020-08-30 17:41:39 +0700

    add setup.sh to setup project using the repo

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+# $1 --> project name 
+# $2 --> package name
+
+git clone git@github.com:typical-go/rest-project-starter.git $1
+rm -rf $1/.git
+rm -rf $1/install.sh $1/README.md
+find $1 -name '*.go' -exec sed -i '' "s+github.com/typical-go/rest-project-starter+$2+g" {} \;
+find $1 -name '*.go' -exec sed -i '' "s+rest-project-starter+$1+g" {} \;
+mv $1/cmd/rest-project-starter $1/cmd/$1
